ec8d40ddbfb4c5cfc2ab97ec7bc9150d89d5f43d
Zack McKenna <zackmckenna@gmail.com>
2020-12-17 12:42:59 -0500

    static site initial

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 const config = {
+  blogName: "Blog",
+  blogDescription: "a test blog created from a titny static stie generator",
+  authorName: "Zack McKenna",
+  authorDescription: "a full stack developer based out of Philadelphia",
+  authorGithub: "github.com/zackmckenna",
   dev: {
     postsdir: "./content",
     outdir: "./public",
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
-const config = require("./config");
 const postMethods = require("./posts");
+const config = require("./config");
+const addHomePage = require("./homepage");
 
 const posts = fs
   .readdirSync(config.dev.postsdir)
   .map((post) => post.slice(0, -3))
-  .map((post) => postMethods.createPost(post));
+  .map((post) => postMethods.createPost(post))
+  .sort(function (a, b) {
+    return b.attributes.date - a.attributes.date;
+  });
 
 if (!fs.existsSync(config.dev.outdir)) fs.mkdirSync(config.dev.outdir);
 
 postMethods.createPosts(posts);
-
-console.log(posts);
+addHomePage(posts);
